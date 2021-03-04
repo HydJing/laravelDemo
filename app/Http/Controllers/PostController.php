d01b7b417f2e2f8bec37abbd905dd2b713b254af
Wei Jing <jaychou123103@hotmail.com>
2021-03-05 03:41:15 +1300

    initialize the post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,10 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
         
     }
 }
