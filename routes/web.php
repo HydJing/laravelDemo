3f0c82d4516a5d3b30d09f55b726352243b0b7e2
Wei Jing <jaychou123103@hotmail.com>
2021-03-09 02:07:09 +1300

    init post like function with migration

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::post('/posts', [PostController::class, 'store']);
 
+Route::get('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
 
